0f54c65a36b0efb5e431df43c24bd596e47cda5f
Mykola Romashenko <sven4369@gmail.com>
2022-12-27 03:17:54 +0300

    modal, snow, lights, scroll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import'./js/burger'
-import './js/slider'
-import'./js/product-modal'
-import'./js/map-modal'
-import'./js/video-modal'
-import'./js/main-modal';
-import'./js/franchise-modal';
+import './js/burger';
+import './js/slider';
+import './js/product-modal';
+import './js/map-modal';
+import './js/video-modal';
+import './js/main-modal';
+import './js/franchise-modal';
